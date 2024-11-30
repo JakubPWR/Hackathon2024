a77c3718c30c5b2d10df2690e930e45a1e0d476e
Mikołaj Musioł <96127814+mikolajmusiol@users.noreply.github.com>
2024-11-30 17:12:34 +0100

    Added Activities table (#9)

--- hackathon-2024/server/Hackaton2024.API/Migrations/20241130155427_Added_Activities_Table.Designer.cs
+++ hackathon-2024/server/Hackaton2024.API/Migrations/20241130155427_Added_Activities_Table.Designer.cs
@@ -4,6 +4,7 @@ using Hackaton2024.API.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Hackaton2024.API.Migrations
 {
     [DbContext(typeof(HackatonDbContext))]
-    partial class HackatonDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241130155427_Added_Activities_Table")]
+    partial class Added_Activities_Table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
