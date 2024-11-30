b7ecfa680600952a71cc0e5a9138e9dad1b19bba
qznikk <115718297+qznikk@users.noreply.github.com>
2024-11-30 18:15:22 +0100

    a

--- hackathon-2024/src/App.jsx
+++ hackathon-2024/src/App.jsx
@@ -6,8 +6,6 @@ import { Login } from "./components/Login";
 import FindNewHobby from "./components/FindNewHobby";
 import UserProfile from "./components/UserProfile";
 import Footer from "./components/Footer";
-import UserProfile from "./components/UserProfile";
-import Footer from "./components/Footer";
 import Settings from "./components/Settings";
 import "./index.css";
 
@@ -30,45 +28,36 @@ function App() {
   }, [theme, fontSize]);
 
   return (
-    <>
-      <AppContext.Provider
-        value={{
-          userId,
-          setUserId,
-          logged,
-          setLogged,
-          theme,
-          setTheme,
-          fontSize,
-          setFontSize,
-        }}
-      >
-      <AppContext.Provider value={{ userId, setUserId, logged, setLogged }}>
-        <Header />
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/landingPage" element={<LandingPage />} />
-          <Route path="/about" element={<h1>About Page</h1>} />
-          <Route path="/settings" element={<Settings />} />
+    <AppContext.Provider
+      value={{
+        userId,
+        setUserId,
+        logged,
+        setLogged,
+        theme,
+        setTheme,
+        fontSize,
+        setFontSize,
+      }}
+    >
+      <Header />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/landingPage" element={<LandingPage />} />
+        <Route path="/about" element={<h1>About Page</h1>} />
+        <Route path="/settings" element={<Settings />} />
 
-          <Route path="/find-hobby" element={<FindNewHobby />} />
-          <Route path="/user-profile" element={<UserProfile />} />
-          <Route
-            path="/tutorial/photography"
-            element={<h1>Fotografia Tutorial</h1>}
-          />
-          <Route
-            path="/tutorial/drawing"
-            element={<h1>Rysowanie Tutorial</h1>}
-          />
-          <Route
-            path="/tutorial/cooking"
-            element={<h1>Gotowanie Tutorial</h1>}
-          />
-        </Routes>
-        <Footer />
-      </AppContext.Provider>
-    </>
+        <Route path="/find-hobby" element={<FindNewHobby />} />
+        <Route path="/user-profile" element={<UserProfile />} />
+        <Route
+          path="/tutorial/photography"
+          element={<h1>Fotografia Tutorial</h1>}
+        />
+        <Route path="/tutorial/drawing" element={<h1>Rysowanie Tutorial</h1>} />
+        <Route path="/tutorial/cooking" element={<h1>Gotowanie Tutorial</h1>} />
+      </Routes>
+      <Footer />
+    </AppContext.Provider>
   );
 }
 
