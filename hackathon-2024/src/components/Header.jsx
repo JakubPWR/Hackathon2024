abb69c108b4f49e5fbaaaf963da3ea4c5f13bbac
qznikk <115718297+qznikk@users.noreply.github.com>
2024-11-30 13:52:48 +0100

    added h2 header to switch to landing page

--- hackathon-2024/src/components/Header.jsx
+++ hackathon-2024/src/components/Header.jsx
@@ -1,27 +1,30 @@
+import React from "react";
+import { Link } from "react-router-dom";
 import "../styles/Header.css";
 
 function Header() {
   return (
-    <>
-      <header className="header">
-        <div className="header-container">
-          <h2 className="header-logo-h2">Hobbystycznie</h2>
-          <nav className="header-nav">
-            <ul className="nav-list">
-              <li className="nav-item">
-                <a href="#about">Ustawienia</a>
-              </li>
-              <li className="nav-item">
-                <a href="#services">Ranking</a>
-              </li>
-              <li className="nav-item">
-                <a href="#contact">Profil</a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </header>
-    </>
+    <header className="header">
+      <div className="header-container">
+        {/* H2 as a clickable link */}
+        <h2 className="header-logo-h2">
+          <Link to="/">Hobbystycznie</Link>
+        </h2>
+        <nav className="header-nav">
+          <ul className="nav-list">
+            <li className="nav-item">
+              <a href="#about">Ustawienia</a>
+            </li>
+            <li className="nav-item">
+              <a href="#services">Ranking</a>
+            </li>
+            <li className="nav-item">
+              <a href="#contact">Profil</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
   );
 }
 
